80101ef37889155143df887f3d1e850547817c5f
Yusuf <tekinyusufselim@gmail.com>
2025-12-02 22:10:38 +0300

    Add option to change task information in menu

--- Source Files/To-Do-List.cpp
+++ Source Files/To-Do-List.cpp
@@ -25,21 +25,27 @@ int main()
         while (login2 == true)
         {
             cout << "A : Add tasks : " << endl;
+            cout << "D : Delete the tasks : " << endl;
             cout << "I : Important : " << endl;
             cout << "M : Mark as done : " << endl;
             cout << "T : Show the task : " << endl;
+            cout << "C : Change information : " << endl;
             cout << "E : Return the login or singup page : " << endl;
             cout << "What your choice : ";
             char lact;
             cin >> lact;
             if (lact == 'A' || lact == 'a')
                 Tasks::AddTask();
+            else if (lact == 'D' || lact == 'd')
+                Tasks::delete_tasks();
             else if (lact == 'I' || lact == 'i')
                 Tasks::important_taks();
             else if (lact == 'M' || lact == 'm')
                 Tasks::mark_task();
             else if (lact == 'T' || lact == 't')
                 Tasks::show_taks();
+            else if (lact == 'C' || lact == 'c')
+                Change_Information::change_information();
             else if (lact == 'E' || lact == 'e')
                 login2 = false;
         }
