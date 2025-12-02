58ef7f6188830d7bd9a24a293780ecdf0b1335a7
Yusuf <tekinyusufselim@gmail.com>
2025-12-02 22:22:20 +0300

    Refactor Forgetp.cpp to use pointers for vectors

--- Source Files/Forgetp.cpp
+++ Source Files/Forgetp.cpp
@@ -26,7 +26,7 @@ bool is_valid_password2(const string& pass)
 	return has_alpha && has_digit;
 }
 
-void save_formation(vector<string>& username1, vector<string>& mail, vector<string>& password)
+void save_formation(vector<string>* username1 = new vector<string>, vector<string>* mail = new vector<string>, vector<string>* password = new vector<string>)
 {
 	string username2, mail1, password1;
 	ifstream user("users.txt");
@@ -35,9 +35,9 @@ void save_formation(vector<string>& username1, vector<string>& mail, vector<stri
 	else {
 		while (user >> username2 >> mail1 >> password1)
 		{
-			username1.push_back(username2);
-			mail.push_back(mail1);
-			password.push_back(password1);
+			username1->push_back(username2);
+			mail->push_back(mail1);
+			password->push_back(password1);
 		}
 		user.close();
 	}
@@ -45,11 +45,16 @@ void save_formation(vector<string>& username1, vector<string>& mail, vector<stri
 
 void Forgetp::change_pass()
 {
-	vector<string> mail, password, username1;
+	vector<string>* mail = new vector<string>;
+	vector<string>* password = new vector<string>;
+	vector<string>* username1 = new vector<string>;
+
 	string email, pass;
 	int code;
+
 	cout << "Please enter the code : ";
 	cin >> code;
+
 	if (code != Forgetp::rand)
 		cout << "You entered wrong code returning the login or singup page." << endl;
 	else {
@@ -62,10 +67,9 @@ void Forgetp::change_pass()
 				cin >> email;
 			} while (!is_valid_mail2(email));
 			save_formation(username1, mail, password);
-
-			for (int i = 0; i < mail.size(); i++)
+			for (int i = 0; i < mail->size(); i++)
 			{
-				if (email == mail[i])
+				if (email == (*mail)[i])
 					idx = i;
 			}
 		}
@@ -76,13 +80,13 @@ void Forgetp::change_pass()
 			cin >> pass;
 		} while (!is_valid_password2(pass));
 		ofstream user("users.txt");
-		for (int i = 0; i < username1.size(); i++)
+		for (int i = 0; i < username1->size(); i++)
 		{
-			user << username1[i] << " " << mail[i] << " ";
+			user << (*username1)[i] << " " << (*mail)[i] << " ";
 			if (idx == i)
 				user << pass << endl;
 			else
-				user << password[i] << endl;
+				user << (*password)[i] << endl;
 		}
 		user.close();
 	}
