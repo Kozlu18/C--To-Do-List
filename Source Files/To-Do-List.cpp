92c5b88303cca86bad5d72103920c2277560884a
Yusuf <tekinyusufselim@gmail.com>
2025-12-07 10:22:05 +0300

    Add settings functionality and improve user flow

--- Source Files/To-Do-List.cpp
+++ Source Files/To-Do-List.cpp
@@ -4,9 +4,23 @@
 #include "login.h"
 #include "Tasks.h"
 #include "Change_Information.h"
+#include "Settings.h"
 #include <iostream>
 
 using namespace std;
+
+void Clear()
+{
+#if defined _WIN32
+    system("cls");
+    //clrscr(); // including header file : conio.h
+#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    system("clear");
+    //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
+#elif defined (__APPLE__)
+    system("clear");
+#endif
+}
 
 int main()
 {
@@ -15,26 +29,33 @@ int main()
     {
         bool login2 = false;
         cout << "C : Create account or L : Login account or E : exit console." << endl;
+
         char act;
         cin >> act;
+
         if (act == 'c' || act == 'C')
             Login::SingUp();
         else if (act == 'L' || act == 'l')
             login2 = Login::login();
+        else if (act == 'E' || act == 'e')
+            siteexit = true;
         else
             cout << "You entered wrong key" << endl;
-        while (login2 == true)
+        while (login2 && !Change_Information::userExit)
         {
+            Clear();
             cout << "A : Add tasks : " << endl;
             cout << "D : Delete the tasks : " << endl;
             cout << "I : Important : " << endl;
             cout << "M : Mark as done : " << endl;
             cout << "T : Show the task : " << endl;
-            cout << "C : Change information : " << endl;
+            cout << "S : Settings : " << endl;
             cout << "E : Return the login or singup page : " << endl;
             cout << "What your choice : ";
+
             char lact;
             cin >> lact;
+
             if (lact == 'A' || lact == 'a')
                 Tasks::AddTask();
             else if (lact == 'D' || lact == 'd')
@@ -45,11 +66,12 @@ int main()
                 Tasks::mark_task();
             else if (lact == 'T' || lact == 't')
                 Tasks::show_taks();
-            else if (lact == 'C' || lact == 'c')
-                Change_Information::change_information();
+            else if (lact == 'S' || lact == 's')
+                Settings::settings();
             else if (lact == 'E' || lact == 'e')
                 login2 = false;
+            else
+                cout << "You entered wrong key, Please try again." << endl;
         }
     }
 }
-
