81f8e2080897efdba191832249d412afa32fe087
Yusuf <tekinyusufselim@gmail.com>
2025-12-20 22:16:45 +0300

    Refactor Settings.cpp to include NTS management
    Added functionality to manage 'NTS' status in settings.

--- Source Files/Settings.cpp
+++ Source Files/Settings.cpp
@@ -6,8 +6,9 @@
 
 using namespace std;
 
-bool Settings::dtf = false; // silmeden önce onayla
-bool dtf_file;
+ // silmeden Ã¶nce onayla
+bool dtf_file, nts_file;// mst = Move starred tasks to the top
+
 
 void Clears()
 {
@@ -26,18 +27,37 @@ void vdtf()
 {
 	string setting = (Login::username_file2 + "_settings") + ".txt";
 	ofstream settingf(setting);
-	if (Settings::dtf == false)
+	if (dtf_file == false)
 	{
-		Settings::dtf = true;
-		cout << "Your status(0 = false, 1 = true) : " << Settings::dtf << endl;
-		settingf << Settings::dtf;
+		dtf_file = true;
+		Settings::dtf = dtf_file;
+		settingf << "DTF : " << dtf_file << " NTS : " << nts_file << endl;
+		cout << "Your status(0 = false, 1 = true) : " << dtf_file << endl;
 		settingf.close();
 	}
 	else
 	{
-		Settings::dtf = false;
-		cout << "Your status(0 = false, 1 = true) : " << Settings::dtf << endl;
-		settingf << Settings::dtf;
+		dtf_file = false;
+		Settings::dtf = dtf_file;
+		settingf << "DTF : " << dtf_file << " NTS : " << nts_file << endl;
+		cout << "Your status(0 = false, 1 = true) : " << dtf_file << endl;
+		settingf.close();
+	}
+}
+
+void vnst()
+{
+	string setting = (Login::username_file2 + "_settings") + ".txt";
+	ofstream settingf(setting);
+	if(nts_file)
+	{
+		nts_file = false;
+		settingf << "DTF : " << dtf_file << " NTS : " << nts_file << endl;
+		settingf.close();
+	}
+	else{
+		nts_file = true;
+		settingf << "DTF : " << dtf_file << " NTS : " << nts_file << endl;
 		settingf.close();
 	}
 }
@@ -45,21 +65,22 @@ void vdtf()
 void Settings::settings()
 {
 	bool setexit = false;
-
 	while (!setexit)
 	{
 		Clears();
 		string setting = (Login::username_file2 + "_settings") + ".txt";
 		ifstream settingf(setting);
+		string empt;
 
-		while (settingf >> dtf_file);
+		while (settingf >> empt >> empt >> dtf_file >> empt >> empt >>nts_file);
 		settingf.close();
 
 		cout << "C : Change personal information : " << endl;
 		cout << "D : Confirm before deleting. Your status(0 = false, 1 = true) : " << dtf_file << endl;
+		cout << "S : Move starred tasks to the top : Your status(0 = false, 1 = true) : " << nts_file << endl;
 		cout << "E : Exit settings : " << endl;
 
-		char sact; // ayarlar yapýlacak olay
+		char sact; // ayarlar yapÄ±lacak olay
 		cin >> sact;
 
 		if (sact == 'C' || sact == 'c')
@@ -69,6 +90,8 @@ void Settings::settings()
 		}
 		else if (sact == 'D' || sact == 'd')
 			vdtf();
+		else if (sact == 'S' || sact == 's')
+			vnst();
 		else if (sact == 'E' || sact == 'e')
 			setexit = true;
 	}
