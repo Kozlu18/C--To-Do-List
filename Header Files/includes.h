9f0820ad57537c6bdbda5fe91d74b0ea4444f7e6
Yusuf <tekinyusufselim@gmail.com>
2025-12-21 21:54:24 +0300

    Added includes.h file

---
+++ Header Files/includes.h
@@ -0,0 +1,16 @@
+#ifndef INCLUDES_H  
+#define INCLUDES_H
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <thread>
+#include <chrono>
+
+#include "Tasks.h"
+#include "Login.h"
+#include "Settings.h"
+
+#endif
